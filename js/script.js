49035a9a2607713e6c31ba26f4c1ac8fda42de05
Montanah <mititi.isaac@gmail.com>
2024-01-24 16:54:41 +0300

    Script.js swipe function addition

--- js/script.js
+++ js/script.js
@@ -130,3 +130,14 @@ document.addEventListener('DOMContentLoaded', function () {
     window.handlePrevList = handlePrevList;
     window.handleNextList = handleNextList;
 });
+
+document.addEventListener('DOMContentLoaded', function () {
+    var swiper = new Swiper('.swiper-container', {
+      slidesPerView: 1,
+      spaceBetween: 10,
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+    });
+  });
